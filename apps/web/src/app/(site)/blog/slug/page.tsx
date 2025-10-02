9cbe2ada37289d5c9ad49d7e484f950f897bae71
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-10-02 20:03:01 +0300

    Made Changes to the site to fix deployment issues

---
+++ apps/web/src/app/(site)/blog/slug/page.tsx
@@ -0,0 +1,13 @@
+const BlogDetailPage = () => {
+
+    return (
+        <div className="blog-detail-page">
+
+            <h2>
+                Hello Blog Detail Page
+            </h2>
+        </div>
+    )
+}
+
+export default BlogDetailPage
