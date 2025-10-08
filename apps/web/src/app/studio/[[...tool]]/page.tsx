6ae5053c6dee822b55578278e7dbdfe6cf43bff6
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-10-08 09:42:29 +0300

    Removed sanity studio from the build process to see if the site will deploy

--- apps/web/src/app/studio/[[...tool]]/page.tsx
+++ apps/web/src/app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,28 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+"use client";
 
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
 
-export const dynamic = "force-static";
-
-export { metadata, viewport } from "next-sanity/studio";
-
 export default function StudioPage() {
+  if (process.env.NODE_ENV === "production") {
+    return (
+      <div
+        style={{
+          display: "flex",
+          height: "100vh",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          fontFamily: "sans-serif",
+        }}
+      >
+        <h1>🛑 Studio Disabled in Production</h1>
+        <p style={{ color: "#888" }}>
+          The Sanity Studio is available only in local development.
+        </p>
+      </div>
+    );
+  }
+
   return <NextStudio config={config} />;
 }
